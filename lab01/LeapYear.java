efad60b4ab66d4cedfe0b34fd56e392ec0911dd8
milnorms <minutemili@gmail.com>
2023-04-07 08:44:49 -1000

    completed leap year

--- lab01/LeapYear.java
+++ lab01/LeapYear.java
@@ -9,6 +9,25 @@ public class LeapYear {
      */
     public static boolean isLeapYear(int year) {
         // Optional TODO: Fill in this method.
+        // Leap Years are defined as a year divisible by 400
+        // or divisible by 4
+        // and not by 100
+        //. For example, 2000 and 2004 are leap years. 1900, 2003, and 2100 are not leap years.
+
+        boolean isLeap;
+
+        if (year % 400 == 0) {
+            isLeap = true;
+        } else if ((year % 4 == 0) & (year % 100 != 0)) {
+            isLeap = true;
+        }
+        else {
+            isLeap = false;
+        }
+
+        return isLeap;
+
+
     }
 
     /** Calls isLeapYear to print correct statement. */
